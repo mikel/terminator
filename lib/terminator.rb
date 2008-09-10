ec37879cca28e5ef7aca1ef5c147ff5b7bb2bf01
ara.t.howard <ara.t.howard@gmail.com>
2008-09-10 05:57:33 +0000

    import terminator-0.4.2
    git-svn-id: svn://rubyforge.org/var/svn/codeforpeople/terminator/trunk@678 29922619-e960-4932-8d85-0e0be7f1775a

---
+++ lib/terminator.rb
@@ -0,0 +1,103 @@
+require 'rbconfig'
+require 'tempfile'
+
+require 'fattr'
+
+module Terminator
+  Version = '0.4.2'
+
+  def terminate options = {}, &block
+    options = { :seconds => Float(options).to_i } unless Hash === options 
+
+    seconds = getopt :seconds, options
+    trap = getopt :trap, options, lambda{ eval("raise(::Terminator::Error, '#{ seconds }s')", block) }
+
+    handler = Signal.trap(signal, &trap)
+
+    plot_to_kill pid, :in => seconds, :with => signal
+
+    begin
+      block.call
+    ensure
+      Signal.trap(signal, handler)
+    end
+  end
+
+  def plot_to_kill pid, options = {}
+    seconds = getopt :in, options
+    signal = getopt :with, options
+    process.puts [pid, seconds, signal].join(' ')
+    process.flush
+  end
+
+  fattr :process do
+    process = IO.popen "#{ ruby } #{ program.inspect }", 'w+'
+    at_exit do
+      begin
+        Process.kill -9, process.pid
+      rescue Object
+      end
+    end
+    process.sync = true
+    process
+  end
+
+  fattr :program do
+    code = <<-code
+      while(( line = STDIN.gets ))
+        pid, seconds, signal, *ignored = line.strip.split
+
+        pid = Float(pid).to_i
+        seconds = Float(seconds)
+        signal = Float(signal).to_i rescue String(signal)
+
+        sleep seconds
+
+        begin
+          Process.kill signal, pid
+        rescue Object
+        end
+      end
+    code
+    tmp = Tempfile.new "#{ ppid }-#{ pid }-#{ rand }" 
+    tmp.write code
+    tmp.close
+    tmp.path
+  end
+
+  fattr :ruby do
+    c = ::Config::CONFIG
+    ruby = File::join(c['bindir'], c['ruby_install_name']) << c['EXEEXT']
+    raise "ruby @ #{ ruby } not executable!?" unless test(?e, ruby)
+    ruby
+  end
+
+  fattr :pid do
+    Process.pid
+  end
+
+  fattr :ppid do
+    Process.ppid
+  end
+
+  fattr :signal do
+    'TERM'
+  end
+
+  def getopt key, hash, default = nil
+    return hash.fetch(key) if hash.has_key?(key)
+    key = key.to_s
+    return hash.fetch(key) if hash.has_key?(key)
+    key = key.to_sym
+    return hash.fetch(key) if hash.has_key?(key)
+    default
+  end
+
+
+
+  class Error < ::StandardError; end
+  def error() ::Terminator::Error end
+  def version() ::Terminator::Version end
+
+  extend self
+end
